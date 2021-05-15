aa90b7341a842a5abc54b7809762b750ab7ea136
“jungkeunmo“ <abc59684495@gmail.com>
2021-05-15 16:12:44 +0900

    crud_6

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -2,15 +2,45 @@ import Book from "../models/Book";
 import Author from "../models/Author";
 
 export const homeController = async(req, res) => {
-    try{
-        const books = await Book.find().populate({
-            path : `author`,
-            model: Author,
-        });
 
-        res.render("screens/home", { books: books });
+    const {
+        query: { seq, searchValue },
+    } = req;
+    
+    try{
+
+        if(!seq && !searchValue) {
+            const books = await Book.find().populate({
+                path : `author`,
+                model: Author,
+            });
+        } else {
+            if(seq === "title") {
+                const books = await Book.find({
+                    title : {$regex : `.*${searchValue}.*`},
+                }).populate({
+                    path: `auhor`,
+                    model: Author,
+                });
+                res.render("screens/home", { books: books });
+            } else if (seq === "author") {
+                const books = await Book.find().populate({
+                    path : `author`,
+                    model: Author,
+                    match : {
+                        name: { $regex: `.*${searchValue}.*`},
+                    },
+                });
+
+                const nexBooks = books.filter((data) => data.author !== null);
+
+                res.render("screens/home", { books: nexBooks });
+
+            }
+        }
+        
     } catch (e) {
-        console.log(e)
+        console.log(e);
         res.render("screens/home", { books: [] });
     }
 };
